e298ec66b0fa7b08ab0048e66947bcf4ce6c2b89
Fajran Iman Rusadi <fajran@gmail.com>
2012-10-12 17:13:06 +0200

    ubah instance variable

---
+++ P2/oop/04b.py
@@ -0,0 +1,24 @@
+class SegiEmpat(object):
+    def __init__(self, panjang, lebar):
+        self.panjang = panjang
+        self.lebar = lebar
+
+    def luas(self):
+        return self.panjang * self.lebar
+
+    def keliling(self):
+        return 2 * self.panjang + 2 * self.lebar
+
+kotak = SegiEmpat(5, 30)
+print 'Panjang:', kotak.panjang
+print 'Lebar:', kotak.lebar
+print 'Luas:', kotak.luas()
+print 'Keliling:', kotak.keliling()
+
+kotak.panjang = 10
+kotak.lebar = 2
+
+print 'Panjang:', kotak.panjang
+print 'Lebar:', kotak.lebar
+print 'Luas:', kotak.luas()
+print 'Keliling:', kotak.keliling()
